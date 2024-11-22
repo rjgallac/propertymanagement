fc8a8c2e2bd0a4a5d726d18337b3198426fc060b
rjgallac <rjgallac@hotmail.com>
2024-11-22 15:03:23 +0000

    add router and vuetify

--- manageapartmentsfrontend/src/App.vue
+++ manageapartmentsfrontend/src/App.vue
@@ -1,44 +1,11 @@
-<script setup>
-import ManageProperties from './components/ManageProperties.vue'
-import ManageTenants from './components/ManageTenants.vue'
-
-import { useApartmentStore } from '@/stores/apartmentStore';
-import { usePropertyStore } from '@/stores/propertyStore';
-import { useBookingStore } from '@/stores/bookingStore';
-import { useTenantStore } from '@/stores/tenantStore';
-import { useLandlordStore } from '@/stores/landlordStore';
-
-const bookingStore = useBookingStore();
-const tenantStore = useTenantStore();
-const apartmentStore = useApartmentStore();
-const propertyStore = usePropertyStore();
-const landlordStore = useLandlordStore();
-
-bookingStore.fetchBookings();
-apartmentStore.fetchApartments();
-tenantStore.fetchTenants();
-landlordStore.fetchLandlords();
-propertyStore.fetchProperties();
-
-
-
-</script>
-
 <template>
-  <header>
-    <div class="wrapper">
-      <ManageProperties />
-    </div>
-  </header>
-
-  <main>
-    <ManageTenants />
-  </main>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-</style>
+<script lang="ts" setup>
+  //
+</script>
